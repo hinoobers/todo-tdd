f28113d534be13571e19f2d7a5fc2bc7f82d23a7
Reio <reio.poldmaa@voco.ee>
2025-04-25 09:28:21 +0300

    Create error handling for unit and integration tests

--- tests/integration/todo.controller.int.test.js
+++ tests/integration/todo.controller.int.test.js
@@ -13,4 +13,13 @@ describe(endpoint, () => {
         expect(response.body.title).toBe(newTodo.title);
         expect(response.body.done).toBe(newTodo.done);
     });
+    it("should return error 500 on malformed data with POST" + endpoint, async () => {
+        const response = await request(app)
+            .post(endpoint)
+            .send({title: "Missing done property"});
+        expect(response.statusCode).toBe(500);
+        expect(response.body).toStrictEqual({
+            message: "Todo validation failed: done: Path `done` is required.",
+        })
+    });
 });
